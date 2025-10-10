0ea82d073ed1d4068995e8341382f85add45ab15
William Sousa <williamrsousa94@hotmail.com>
2025-10-09 21:28:25 -0300

    Made some adjustments on page width and bottom alignment

--- src/components/NavigationMenu/index.jsx
+++ src/components/NavigationMenu/index.jsx
@@ -2,7 +2,7 @@ import "./navigation-menu.style.css";
 
 export function NavigationMenu({ children }) {
   return (
-    <nav>
+    <nav className="navigation">
       <ul className="navigation-menu-list">{children}</ul>
     </nav>
   );
